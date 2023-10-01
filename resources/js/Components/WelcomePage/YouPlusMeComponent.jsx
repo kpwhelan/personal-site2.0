dd873352269be6fe91a68d196a23af280627c28a
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2023-10-01 19:29:46 -0400

    breakpoints for icon section

--- resources/js/Components/WelcomePage/YouPlusMeComponent.jsx
+++ resources/js/Components/WelcomePage/YouPlusMeComponent.jsx
@@ -5,30 +5,58 @@ export default function YouPlusMeComponent() {
     const opacityValue = 0.7;
 
     return (
-        <div className="flex items-center justify-between md:justify-around mt-10  mx-auto">
-            <div className="text-center">
-                <FontAwesomeIcon icon={faPersonRays} size="8x" opacity={opacityValue}/>
-                <p className="text-4xl">You</p>
+        <>
+            <div className="hidden md:flex items-center justify-around mt-10 mx-auto">
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faPersonRays} size="8x" opacity={opacityValue}/>
+                    <p className="text-4xl">You</p>
+                </div>
+
+                <div>
+                    <FontAwesomeIcon icon={faPlus} size="3x" opacity={opacityValue}/>
+                </div>
+
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faPerson} size="8x" opacity={opacityValue} />
+                    <FontAwesomeIcon icon={faLaptop} size="4x" opacity={opacityValue} />
+                    <p className="text-4xl">Me</p>
+                </div>
+
+                <div>
+                    <FontAwesomeIcon icon={faEquals} size="3x" opacity={opacityValue} />
+                </div>
+
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faExplosion} size="8x" opacity={opacityValue}/>
+                    <p className="text-4xl">Magic</p>
+                </div>
             </div>
 
-            <div>
-                <FontAwesomeIcon icon={faPlus} size="3x" opacity={opacityValue}/>
-            </div>
+            <div className="flex md:hidden items-center justify-around mt-10 mx-auto">
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faPersonRays} size="4x" opacity={opacityValue}/>
+                    <p className="text-4xl">You</p>
+                </div>
 
-            <div className="text-center">
-                <FontAwesomeIcon icon={faPerson} size="8x" opacity={opacityValue} />
-                <FontAwesomeIcon icon={faLaptop} size="4x" opacity={opacityValue} />
-                <p className="text-4xl">Me</p>
-            </div>
+                <div>
+                    <FontAwesomeIcon icon={faPlus} size="1x" opacity={opacityValue}/>
+                </div>
 
-            <div>
-                <FontAwesomeIcon icon={faEquals} size="3x" opacity={opacityValue} />
-            </div>
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faPerson} size="4x" opacity={opacityValue} />
+                    <FontAwesomeIcon icon={faLaptop} size="4x" opacity={opacityValue} />
+                    <p className="text-4xl">Me</p>
+                </div>
 
-            <div className="text-center">
-                <FontAwesomeIcon icon={faExplosion} size="8x" opacity={opacityValue}/>
-                <p className="text-4xl">Magic</p>
+                <div>
+                    <FontAwesomeIcon icon={faEquals} size="1x" opacity={opacityValue} />
+                </div>
+
+                <div className="text-center">
+                    <FontAwesomeIcon icon={faExplosion} size="4x" opacity={opacityValue}/>
+                    <p className="text-4xl">Magic</p>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
