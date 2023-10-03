6c91aabb17e3f7864f49e78c8f7cdf179016bfee
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2023-10-03 12:18:52 -0400

    adjust footer padding

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -10,7 +10,7 @@ import {
 
 export default function Footer({ className }) {
     return (
-        <div className={`flex justify-between items-center p-4 border-t-2 border-t-my-sage border-b-2 border-b-my-sage mb-2 mt-36 ${className}`}>
+        <div className={`flex justify-between items-center p-2 md:p-4 border-t-2 border-t-my-sage border-b-2 border-b-my-sage mb-2 mt-36 ${className}`}>
             <p className="text-xs">copyright &copy; {new Date().getFullYear()} · ALL RIGHTS RESERVED BY Kevin Whelan - TNSG Web Development</p>
             <div className="hidden md:block">
                 <a href="https://www.instagram.com/kevpw_dev/" target="_blank" className="mx-2"><FontAwesomeIcon icon={faInstagram} size={'2x'} /></a>
