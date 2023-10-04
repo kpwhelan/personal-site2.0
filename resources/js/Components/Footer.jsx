bb993f9234360171c1b93a0dd4e37b923085bd70
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2023-10-03 20:42:52 -0400

    adjust flex for footer when on mobile

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -10,18 +10,18 @@ import {
 
 export default function Footer({ className }) {
     return (
-        <div className={`flex justify-between items-center p-2 md:p-4 border-t-2 border-t-my-sage border-b-2 border-b-my-sage mb-2 mt-36 ${className}`}>
+        <div className={`flex flex-col-reverse md:flex-row justify-between items-center p-2 md:p-4 border-t-2 border-t-my-sage border-b-2 border-b-my-sage mb-2 mt-36 ${className}`}>
             <p className="text-xs">copyright &copy; {new Date().getFullYear()} · ALL RIGHTS RESERVED BY Kevin Whelan</p>
-            <div className="hidden md:block">
+            <div className="mb-2 md:mb-0">
                 <a href="https://www.instagram.com/kevpw_dev/" target="_blank" className="mx-2"><FontAwesomeIcon icon={faInstagram} size={'2x'} /></a>
                 <a href="https://twitter.com/kevpw_dev" target="_blank" className="mx-2"><FontAwesomeIcon icon={faXTwitter} size={'2x'} /></a>
                 <a href="https://linkedin.com/in/kpwhelan" target="_blank" className="mx-2"><FontAwesomeIcon icon={faLinkedin} size={'2x'} /></a>
             </div>
 
-            <div className="md:hidden">
+            {/* <div className="md:hidden">
             <Menu>
                 <MenuHandler>
-                    <Button className="bg-white border-2 border-my-sage text-my-gray">Social Media</Button>
+                    <p className="animate-pulse">Social Media</p>
                 </MenuHandler>
                 <MenuList className="flex justify-between">
                     <MenuItem><a href="https://www.instagram.com/kevpw_dev/" target="_blank" className="mx-2"><FontAwesomeIcon icon={faInstagram} size={'2x'} /></a></MenuItem>
@@ -29,7 +29,7 @@ export default function Footer({ className }) {
                     <MenuItem><a href="https://linkedin.com/in/kpwhelan" target="_blank" className="mx-2"><FontAwesomeIcon icon={faLinkedin} size={'2x'} /></a></MenuItem>
                 </MenuList>
             </Menu>
-            </div>
+            </div> */}
 
         </div>
     )
