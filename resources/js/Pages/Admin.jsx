c907147c8d3669e58ed16b160d9804f119295577
kpwhelan <60331971+kpwhelan@users.noreply.github.com>
2023-10-05 09:46:34 -0400

    styling updates for admin page

--- resources/js/Pages/Admin.jsx
+++ resources/js/Pages/Admin.jsx
@@ -21,27 +21,30 @@ export default function Admin({ records }) {
     return (
         <div className="p-2">
             <div className="w-full border-b-2 border-b-my-sage">
-                <p className="text-3xl">Kevin Whelan - Admin Panel</p>
+                <p className="text-2xl">Kevin Whelan - Admin Panel</p>
             </div>
-            {records.map(record => {
-                return <Card className="w-[95%] md:w-[30%] p-2 my-6" key={record.id}>
-                            <Button onClick={setAsResponded} id={record.id}>Mark Responded</Button>
-                            <CardBody>
-                                <Typography variant="h3" color="blue-gray">
-                                    Name: {record.name}
-                                </Typography>
-                                <Typography variant="h5" color="blue-gray" className="mb-2">
-                                    Subject: {record.subject}
-                                </Typography>
-                                <Typography>
-                                    Email: {record.email}
-                                </Typography>
-                                <Typography>
-                                    Message: {record.message}
-                                </Typography>
-                            </CardBody>
-                        </Card>
-            })}
+            <div className="mt-6">
+                <p className="text-xl">New Contacts</p>
+                {records.map(record => {
+                    return <Card className="w-[95%] md:w-[30%] p-2 my-6" key={record.id}>
+                                <Button onClick={setAsResponded} id={record.id} className="bg-my-sage">Mark Responded</Button>
+                                <CardBody>
+                                    <Typography variant="h3" color="blue-gray">
+                                        Name: {record.name}
+                                    </Typography>
+                                    <Typography variant="h5" color="blue-gray" className="mb-2">
+                                        Subject: {record.subject}
+                                    </Typography>
+                                    <Typography>
+                                        Email: {record.email}
+                                    </Typography>
+                                    <Typography>
+                                        Message: {record.message}
+                                    </Typography>
+                                </CardBody>
+                            </Card>
+                })}
+            </div>
         </div>
     )
 }
